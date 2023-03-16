2c5fc33c32bb54bb9978732d7286e44ce5866888
bsun1220 <65567539+bsun1220@users.noreply.github.com>
2023-03-16 15:38:28 -0400

    func

--- ifopt_test.cpp
+++ ifopt_test.cpp
@@ -11,6 +11,24 @@ using namespace ifopt;
 using namespace std;
 using namespace Eigen;
 
+struct z_result{
+    bool found_sol; 
+    VectorXd z; 
+};
+
+struct fr_result{
+    MatrixXd A;
+    VectorXd b; 
+};
+
+struct problem_result{
+    MatrixXd reduced_A;
+    VectorXd reduced_b; 
+    bool reduced;
+    VectorXd b_tilde;
+    MatrixXd M;
+};
+
 double round_up(double value, int decimal_places) {
     const double multiplier = std::pow(10.0, decimal_places);
     return round(value * multiplier) / multiplier;
@@ -145,9 +163,7 @@ class ExCost : public CostTerm{
     
     double GetCost() const override
     {
-        cout << "hello" << endl;
         VectorXd x = GetVariables()->GetComponent("var_set1")->GetValues();
-        cout << " hello" << endl;
         return x(x.rows() - 1);
     };
     void FillJacobianBlock (string var_set, Jacobian& jac_block) const override{}
@@ -159,53 +175,11 @@ MatrixXd equal_conversion(MatrixXd A){
     return A_tilde;
 }
 
-void find_z(MatrixXd A, VectorXd b, int x_dim){
-    int n = A.rows();
-    int d = A.cols();
-    for(int i = x_dim; i < d; i++){
-        cout << "hi" << endl;
-    }
-}
-
-void reduce_problem(MatrixXd A, VectorXd b){
-    MatrixXd newA = equal_conversion(A);
-    find_z(newA, b, A.cols());
-}
-
-int main(){
-    MatrixXd A (6,3);
-    A << 1,0,0,-1,0,0,0,1,0,0,-1,0,0,0,1,0,0,-1;
-    VectorXd b(6);
-    b << 1,1,1,1,0,0;
-    MatrixXd newA = equal_conversion(A);
+z_result find_z(MatrixXd newA, VectorXd b, int x_dim){
     int n = newA.rows();
     int d = newA.cols();
-    int x_dim = A.cols();
 
-    int ind = 7;
-
-    MatrixXd eqA(x_dim + 2, newA.rows());
-    for(int i = 0; i < x_dim; i++){
-            eqA.row(i) = newA.col(i);
-    }
-    eqA.row(x_dim) = newA.col(ind);
-    eqA.row(x_dim + 1) = b;
-
-    eqA = make_full_rank(eqA);
-
-    
-    VectorXd eqb = VectorXd::Zero(eqA.rows());
-    eqb(x_dim) = 1;
-
-    VectorXd init = eqA.inverse() * eqb;
-    double delta = ((-1 * newA).transpose() * init).maxCoeff();
-    VectorXd temp (eqA.rows() + 1);
-    for(int i =0 ; i < eqA.rows(); i++){
-        temp(i) = init(i);
-    }
-    temp(eqA.rows()) = delta;
-    init = temp;
-
+    // setting up ineqA with extra column for delta
     MatrixXd ineqA (newA.rows() + 1, newA.cols());
     for(int i = 0; i < newA.rows(); i++){
         ineqA.row(i) = -1 * newA.row(i);
@@ -219,35 +193,196 @@ int main(){
     MatrixXd temp_m = ineqA.transpose();
     ineqA = temp_m;
 
-    MatrixXd temp_eqA (eqA.rows(), eqA.cols() + 1);
-    for(int i = 0; i < eqA.cols(); i++){
-        temp_eqA.col(i) = eqA.col(i);
+    for(int ind = x_dim; ind < d; ind++){
+        //setting up eqA
+        MatrixXd eqA(x_dim + 2, newA.rows());
+        for(int i = 0; i < x_dim; i++){
+                eqA.row(i) = newA.col(i);
+        }
+        eqA.row(x_dim) = newA.col(ind);
+        eqA.row(x_dim + 1) = b;
+        int b_length = eqA.cols() > x_dim + 2 ? eqA.cols() : x_dim + 2;
+        VectorXd eqb = VectorXd::Zero(b_length);
+        eqb(x_dim) = 1;
+
+        VectorXd init;
+        if (eqA.rows() <= eqA.cols()){
+            eqA = make_full_rank(eqA);
+            FullPivLU <MatrixXd> lu(eqA);
+            //if eqA is not invertible skip the step
+            if (!lu.isInvertible()) continue;
+            //create init with last entry as delta
+            
+            init = eqA.inverse() * eqb;
+        } else {
+            init = eqA.colPivHouseholderQr().solve(eqb);
+        }
+        
+        double delta = ((-1 * newA).transpose() * init).maxCoeff();
+        VectorXd temp (init.rows() + 1);
+        for(int i =0 ; i < init.rows(); i++){
+            temp(i) = init(i);
+        }
+        temp(init.rows()) = delta;
+        init = temp;
+
+        //add one column for delta
+        MatrixXd temp_eqA (eqA.rows(), eqA.cols() + 1);
+        for(int i = 0; i < eqA.cols(); i++){
+            temp_eqA.col(i) = eqA.col(i);
+        }
+       
+        temp_eqA.col(eqA.cols()) = VectorXd::Zero(eqA.rows());
+        eqA = temp_eqA;
+
+        string name = "var_set1";
+        Problem lp;
+        lp.AddVariableSet(make_shared<ExVariables>(n + 1, name, init));
+        lp.AddConstraintSet(make_shared<ExConstraint1>(ineqA.rows(),name,ineqA));
+        lp.AddConstraintSet(make_shared<ExConstraint2>(eqA.rows(),name,eqA, eqb));
+        lp.AddCostSet(make_shared<ExCost>(name));
+        IpoptSolver ipopt;
+        ipopt.Solve(lp);
+
+        VectorXd sol = lp.GetOptVariables()->GetValues();
+
+        if (sol(sol.rows() - 1) <= 0){
+            VectorXd ans_v (sol.rows() - 1);
+            for(int i = 0; i < ans_v.rows(); i++){
+                ans_v(i) = sol(i);
+            }
+            z_result ans;
+            ans.found_sol = true; 
+            ans.z = (newA.transpose() * ans_v);
+            return ans;
+        }
     }
-    temp_eqA.col(eqA.cols()) = VectorXd::Zero(eqA.rows());
-    eqA = temp_eqA;
+    z_result ans;
+    ans.found_sol = false;
+    return ans;
+}
 
-    cout << eqA * init << endl;
-    cout << "--------" << endl;
-    cout << eqb << endl;
-    cout << "----------" << endl;
-    cout << ineqA * init << endl;
+MatrixXd facial_reduction(VectorXd z){
+    int d = z.rows();
+    vector<int> indices;
+    for(int i = 0; i < d; i++){
+        z(i) = round_up(z(i), 5);
+        if(z(i) <= 0) indices.push_back(i);
+    }
+    MatrixXd matrix (indices.size(), d);
+    for(int i = 0; i < indices.size(); i++){
+        VectorXd row = VectorXd::Zero(d);
+        row(indices[i]) = 1; 
+        matrix.row(i) = row;
+    }
+    return matrix.transpose();
+}
+
+fr_result entire_facial_reduction_step(MatrixXd A, VectorXd b, int x_dim){
+    z_result z_ans = find_z(A, b, x_dim);
+    if(!z_ans.found_sol){
+        fr_result f_ans;
+        f_ans.A = A;
+        f_ans.b = b;
+        return f_ans;
+    }
+
+    MatrixXd V = facial_reduction(z_ans.z);
+    MatrixXd AV = A * V;
+    FullPivLU<MatrixXd> lu(AV.transpose());
+    MatrixXd rref = lu.matrixLU().triangularView<Upper>();
+    VectorXd max_val = rref.rowwise().maxCoeff();
+    VectorXd min_val = rref.rowwise().minCoeff();
+
+    vector<int> lst; 
+    for(int i = 0; i < max_val.rows(); i++){
+        if (max_val(i) != 0 || min_val(i) != 0){
+            lst.push_back(i);
+        }
+    }
+    MatrixXd proj (lst.size(), A.rows());
+    for(int i = 0; i < lst.size(); i++){
+        VectorXd row = VectorXd::Zero(A.rows());
+        row(lst[i]) = 1; 
+        proj.row(i) = row; 
+    }
+
+    A = proj * AV;
+    b = proj * b;
+    return entire_facial_reduction_step(A, b, x_dim);
+}
+
+fr_result reduce_sampling(MatrixXd M, VectorXd b, int delta_dim){
+    MatrixXd M_inv = M.inverse();
+    int row = M_inv.rows() - delta_dim;
+    int gamma = M_inv.rows() - b.rows();
+
+    MatrixXd temp1(M_inv.rows() - row, M_inv.cols() - gamma);
+    for(int i = 0; i < temp1.rows(); i++){
+        for(int k = 0; k < temp1.cols(); k++){
+            temp1(i, k) = M_inv(i + row, k);
+        }
+    }
+
+    MatrixXd temp2(M_inv.rows() - row, gamma);
+    for(int i = 0; i < temp2.rows(); i++){
+        for(int k = 0; k < temp2.cols(); k++){
+            temp2(i, k) = M_inv(i + row, k + M_inv.cols() - gamma);
+        }
+    }
+    fr_result ans;
+    ans.A = -1 * temp2;
+    ans.b = temp1 * b;
+    return ans;
+}
+
+problem_result reduce_problem(MatrixXd A, VectorXd b){
+    MatrixXd newA = equal_conversion(A);
+    int x_dim = A.cols();
+    fr_result res = entire_facial_reduction_step(newA, b, x_dim);
+    if (res.A.cols() == A.cols() && res.A.rows() == A.rows()){
+        problem_result ans;
+        ans.reduced = false; 
+        ans.reduced_A = A;
+        ans.reduced_b = b;
+        return ans;
+    }
+    MatrixXd M = make_full_rank(res.A);
+    fr_result reduced = reduce_sampling(M, res.b, res.A.cols() - x_dim);
+
+    problem_result ans;
+    ans.reduced = true; 
+    ans.reduced_A = reduced.A;
+    ans.reduced_b = reduced.b;
+    ans.b_tilde = res.b;
+    ans.M = M;
+    return ans;
     
-    cout << "-------" << endl;
-    cout << init << endl;
+}
 
-    string name = "var_set1";
-    Problem lp;
-    lp.AddVariableSet(make_shared<ExVariables>(n + 1, name, init));
-    lp.AddConstraintSet(make_shared<ExConstraint1>(ineqA.rows(),name,ineqA));
-    lp.AddConstraintSet(make_shared<ExConstraint2>(eqA.rows(),name,eqA, eqb));
-    lp.AddCostSet(make_shared<ExCost>(name));
-    IpoptSolver ipopt;
-    ipopt.Solve(lp);
-
-    VectorXd sol = lp.GetOptVariables()->GetValues();
-    cout << sol << endl;
-
-
+int main(){
     
+    /*
+    MatrixXd A (6,3);
+    A << 1, 0, 0, -1, 0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 1, 0, 0, -1;
+    VectorXd b(6);
+    b << 1,1,1,1,0,0;
+    */
+    
+    
+    MatrixXd A (4,2);
+    A << 1, 0, -1, 0, 0, 1, 0, -1;
+    VectorXd b(4);
+    b << 2, 2, 0, 0;
+    
+    problem_result ans = reduce_problem(A, b);
+    cout << ans.reduced_A << endl;
+    cout << "------" << endl;
+    cout << ans.reduced_b << endl;
+    cout << "------" << endl;
+    cout << ans.b_tilde << endl;
+    cout << "------" << endl;
+    cout << ans.M << endl;
+
 
 }
